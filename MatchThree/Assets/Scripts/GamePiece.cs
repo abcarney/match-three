2794870cdb5b284f92a3295cafca2d5311beb07f
abcarney <abcarney@users.noreply.github.com>
2020-06-04 07:20:27 -0700

    Completed step 10 - interpolation

--- MatchThree/Assets/Scripts/GamePiece.cs
+++ MatchThree/Assets/Scripts/GamePiece.cs
@@ -9,6 +9,17 @@ public class GamePiece : MonoBehaviour
 
     bool m_isMoving = false;
 
+    public InterpType interpolation = InterpType.SmootherStep;
+
+    public enum InterpType
+    {
+        Linear,
+        EaseOut,
+        EaseIn,
+        SmoothStep,
+        SmootherStep
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +31,11 @@ public class GamePiece : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Move((int)transform.position.x + 1, (int)transform.position.y, 0.5f);
+            Move((int)transform.position.x + 2, (int)transform.position.y, 0.5f);
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            Move((int)transform.position.x - 1, (int)transform.position.y, 0.5f);
+            Move((int)transform.position.x - 2, (int)transform.position.y, 0.5f);
         }
     }
 
@@ -64,9 +75,28 @@ public class GamePiece : MonoBehaviour
 
             elapsedTime += Time.deltaTime;
 
-            // Clamp just to make sure is within correct bounds
+            // Calculate Lerp value, clamp to make sure is within correct bounds
             float t = Mathf.Clamp(elapsedTime / timeToMove, 0f, 1f);
 
+            switch (interpolation)
+            {
+                case InterpType.Linear:
+                    break;
+                case InterpType.EaseOut:
+                    t = Mathf.Sin(t * Mathf.PI * 0.5f);
+                    break;
+                case InterpType.EaseIn:
+                    t = 1 - Mathf.Cos(t * Mathf.PI * 0.5f);
+                    break;
+                case InterpType.SmoothStep:
+                    t = t*t*(3 - 2*t);
+                    break;
+                case InterpType.SmootherStep:
+                    t = t*t*t*(t*(t*6 - 15) + 10);
+                    break;
+            }
+
+            // Move the game piece
             transform.position = Vector3.Lerp(startPosition, destination, t);
 
             // Wait until next frame

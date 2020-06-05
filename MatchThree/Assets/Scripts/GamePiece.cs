a22fa0b431c2d5b1401b97dae7c8ef6cfa6c8149
abcarney <abcarney@users.noreply.github.com>
2020-06-04 21:07:36 -0700

    Completed step 13 - finding matches

--- MatchThree/Assets/Scripts/GamePiece.cs
+++ MatchThree/Assets/Scripts/GamePiece.cs
@@ -22,6 +22,21 @@ public class GamePiece : MonoBehaviour
         SmootherStep
     }
 
+    public MatchValue matchValue;
+    
+    public enum MatchValue
+    {
+        Yellow,
+        Blue,
+        Magenta,
+        Indigo,
+        Green,
+        Teal,
+        Red,
+        Cyan,
+        Wild
+    }
+
     // Start is called before the first frame update
     void Start()
     {

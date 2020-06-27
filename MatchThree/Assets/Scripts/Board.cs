74fdac823343bcec0b6b1e955e6584c7462f73c8
abcarney <abcarney@users.noreply.github.com>
2020-06-26 20:43:38 -0700

    Completed step 43 - the score manager part 2

--- MatchThree/Assets/Scripts/Board.cs
+++ MatchThree/Assets/Scripts/Board.cs
@@ -47,6 +47,8 @@ public class Board : MonoBehaviour
     public int fillYOffset = 10;
     public float fillMoveTime = 0.5f;
 
+    int m_scoreMultiplier = 0;
+
     [System.Serializable]
     public class StartingObject
     {
@@ -660,6 +662,15 @@ public class Board : MonoBehaviour
             if (piece != null)
             {
                 ClearPieceAt(piece.xIndex, piece.yIndex);
+
+                int bonus = 0;
+                if (gamePieces.Count >= 4)
+                {
+                    bonus = 20;
+                }
+
+                piece.ScorePoints(m_scoreMultiplier, bonus);
+
                 if (m_particleManager != null)
                 {
                     if (bombedPieces.Contains(piece))
@@ -772,8 +783,12 @@ public class Board : MonoBehaviour
         m_playerInputEnabled = false;
         List<GamePiece> matches = gamePieces;
 
+        m_scoreMultiplier = 0;
+
         do
         {
+            m_scoreMultiplier++;
+
             // Clear and Collapse Board
             yield return StartCoroutine(ClearAndCollapseRoutine(matches));
             yield return null;
@@ -864,6 +879,7 @@ public class Board : MonoBehaviour
             }
             else
             {
+                m_scoreMultiplier++;
                 yield return StartCoroutine(ClearAndCollapseRoutine(matches));
             }
         }
